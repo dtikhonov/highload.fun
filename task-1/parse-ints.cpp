4b597e520b2277b00448e21afa133fb2f4282a8d
Dmitri Tikhonov <dtikhonov@live.com>
2024-07-26 18:42:58 -0400

    A different approach, but still only 62,236

--- task-1/parse-ints.cpp
+++ task-1/parse-ints.cpp
@@ -5,84 +5,17 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define FORTY_EIGHT_ZEROS \
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-
-static const int lookup[10]['9' + 1] = {
-    {
-#define F 1
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 10
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 100
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 1'000
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 10'000
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 100'000
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 1'000'000
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 10'000'000
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 100'000'000
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F, 3 * F, 4 * F, 5 * F, 6 * F, 7 * F, 8 * F, 9 * F,
-    },
-    {
-#undef F
-#define F 1'000'000'000
-        FORTY_EIGHT_ZEROS,
-        0, 1 * F, 2 * F,
-    },
-};
-
 int
 main (void)
 {
     char bufs[2][0x1000];
 
     size_t nread;
-    long sum;
+    unsigned counts[10] = {};
     int buf_idx, lu_idx;
     const char *p;
 
-    sum = buf_idx = lu_idx = 0;
+    buf_idx = lu_idx = 0;
 
     bufs[!buf_idx][ sizeof(bufs[0]) - 1 ] = '\n';
 
@@ -100,14 +33,18 @@ main (void)
             if (*p == '\n')
                 lu_idx = 0;
             else
-                sum += lookup[lu_idx++][(int)*p];
+                counts[lu_idx++] += *p - '0';
 
         buf_idx = !buf_idx;
         for (p = bufs[buf_idx] + sizeof(bufs[0]) - 1; *p != '\n'; --p)
-            sum += lookup[lu_idx++][(int)*p];
+            counts[lu_idx++] += *p - '0';
         lu_idx = 0;
     }
     while (nread == sizeof(bufs[0]));
+
+    unsigned long sum = 0;
+    for (int c = 0, factor = 1; c <= 9; ++c, factor *= 10)
+        sum += (unsigned long) counts[c] * (unsigned long) factor;
 
     printf("%ld\n", sum);
 
