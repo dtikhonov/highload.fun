6758354f8c8d8053d6e55ad1f9806b259590233e
Dmitri Tikhonov <dtikhonov@live.com>
2024-07-29 16:19:50 -0400

    Let gcc do its own unrolling.  New score: 25,948

--- task-5/count.cpp
+++ task-5/count.cpp
@@ -25,6 +25,7 @@ main (void)
     const uintptr_t *el, *last;
     uintptr_t v;
     unsigned res;
+    uintptr_t acc;
     struct stat st;
 
     res = 0;
@@ -39,33 +40,41 @@ main (void)
     const char *buf = (char *) mmap(0, st.st_size, PROT_READ,
                             MAP_PRIVATE | MAP_POPULATE, STDIN_FILENO, 0);
     last = (uintptr_t *) (buf + st.st_size);
-    for (el = (uintptr_t *) buf; el + 7 < last; el += 8)
+    el = (uintptr_t *) buf;
+    while (el + 255 < last)
     {
-        uintptr_t acc = 0;
+        const uintptr_t *end = el + 255;
+        acc = 0;
 
-#define DO_ELEM(_idx) do { \
-        v = (el[_idx] & 0x7F7F7F7F7F7F7F7F) + \
-                        ((~el[_idx] & 0x8080808080808080) >> 7); \
-        v &= 0x8080808080808080; \
-        v >>= 7; \
-        acc += v; \
-} while (0)
-
-        DO_ELEM(0);
-        DO_ELEM(1);
-        DO_ELEM(2);
-        DO_ELEM(3);
-        DO_ELEM(4);
-        DO_ELEM(5);
-        DO_ELEM(6);
-        DO_ELEM(7);
+        for ( ; el < end; ++el)
+        {
+            v = (*el & 0x7F7F7F7F7F7F7F7F) +
+                            ((~*el & 0x8080808080808080) >> 7);
+            v &= 0x8080808080808080;
+            v >>= 7;
+            acc += v;
+        }
 
         acc = (acc >> 32) + (acc & 0xFFFFFFFF);
         acc = (acc >> 16) + (acc & 0xFFFF);
         acc = (acc >> 8)  + (acc & 0xFF);
         res += acc;
-
     }
+
+    acc = 0;
+    for ( ; el < last; ++el)
+    {
+        v = (*el & 0x7F7F7F7F7F7F7F7F) +
+                        ((~*el & 0x8080808080808080) >> 7);
+        v &= 0x8080808080808080;
+        v >>= 7;
+        acc += v;
+    }
+
+    acc = (acc >> 32) + (acc & 0xFFFFFFFF);
+    acc = (acc >> 16) + (acc & 0xFFFF);
+    acc = (acc >> 8)  + (acc & 0xFF);
+    res += acc;
 
     printf("%u\n", res);
 
